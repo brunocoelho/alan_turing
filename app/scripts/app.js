f0dc1ab9a456ba8b05c61fe84f5254424641b0db
Bruno Coelho <bcs.cin@gmail.com>
2015-02-02 03:24:53 -0300

    Add autocomplete for address field

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ngAutocomplete'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,10 @@
  * Controller of the toptalApp
  */
 angular.module('toptalApp')
-  .controller('MainCtrl', function (User) {
+  .controller('MainCtrl', function ($scope, User) {
     this.user = User.getProfile();
+
+    $scope.$watch('details', function(details) {
+      console.log(details);
+    });
   });
